011b84c0ba35d8646af14790cbccd4b863741e3c
Ross Foy <hamzaoflouisville@gmail.com>
2021-09-25 21:20:18 -0400

    complete the database-connection file under , hide the MongoURI, and update .gitignore.

---
+++ config/db.js
@@ -0,0 +1,39 @@
+/*::::::::::::::::::::::::::::::::::::::::
+::::::::  Required Dependencies  :::::::::
+::::::::::::::::::::::::::::::::::::::::*/
+
+const mongoose = require('mongoose');
+const config = require('config');
+const dbKey = process.env.MONGODB_URI || config.get('mongoURI');
+
+
+
+/*::::::::::::::::::::::::::::::::::::::::
+:::::  Connection to Mongo Database ::::::
+::::::::::::::::::::::::::::::::::::::::*/
+
+const connectingDB = async () => {
+    try {
+        await mongoose.connect(
+            dbKey,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            }
+        );
+
+        console.log(`This spaceship has successfully connected with MongoDB. . .`);
+
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+};
+
+
+
+/*::::::::::::::::::::::::::::::::::::::::
+::: Exporting Database Connection Func :::
+::::::::::::::::::::::::::::::::::::::::*/
+
+module.exports = connectingDB;
