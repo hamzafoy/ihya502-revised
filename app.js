123ea3e2bf276091bdadd23e182a6d5f73eb4c16
Ross Foy <hamzaoflouisville@gmail.com>
2021-09-25 20:59:22 -0400

    adding more necessary code to root app.js

--- app.js
+++ app.js
@@ -18,3 +18,20 @@ const application = express();
 const connectingDB = require('./config/db');
 const routes = require('./routes/visitorInput');
 const port = process.env.PORT || 8080;
+
+
+
+/*::::::::::::::::::::::::::::::::::::::::
+::::::::::::  Backend Tasks  :::::::::::::
+::::::::::::::::::::::::::::::::::::::::*/
+
+connectingDB();
+application.use(cors({
+    origin: true,
+    credentials: true
+}));
+application.use(express.json({
+    extended: false
+}));
+application.use('/api', routes);
+application.listen(port, () => console.log(`This funky DJ is spinnin' tracks on port ${port}`));
