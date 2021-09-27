e51ab1f3535d1d98f10efab376a7dc62789b290f
Ross Foy <hamzaoflouisville@gmail.com>
2021-09-26 21:13:45 -0400

    continuing to work on animations of the play/pause button on landing page.

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -26,10 +26,10 @@ class LandingPage extends React.Component {
                     PODCAST FOR LOUISVILLE MUSLIMS
                 </h1>
 
+                <section className="landing-page__animated-button">
+
                 <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="100"
-                height="100"
                 fill="none"
                 stroke="currentColor"
                 strokeLinecap="round"
@@ -38,9 +38,11 @@ class LandingPage extends React.Component {
                 viewBox="0 0 24 24"
                 className="svg__play"
                 >
-                    <circle class="svg__play-circle" cx="12" cy="12" r="8"></circle>
+                    <circle class="svg__play-circle" cx="50%" cy="50%" r="11.5"></circle>
                     <path class="svg__play-button" d="M10 8L16 12 10 16 10 8z"></path>
                 </svg>
+
+                </section>
 
             </div>
         )
