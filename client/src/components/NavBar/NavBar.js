1ba963b0de4e6bd5b1c233c8392da1f2bccb6780
Ross Foy <hamzaoflouisville@gmail.com>
2021-09-29 21:56:21 -0400

    add URL links to social media nav icons in navbar.

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -20,58 +20,76 @@ class NavBar extends React.Component {
 
                 <section className="nav-bar__social-icons">
 
-                    <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="nav-bar__svg svg-instagram"
-                    viewBox="0 0 24 24"
+                    <a 
+                    target="_blank" 
+                    rel="noreferrer" 
+                    href="https://www.instagram.com/ihya502/"
                     >
-                        <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-                        <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37z"></path>
-                        <path d="M17.5 6.5L17.51 6.5"></path>
-                    
-                    </svg>
-
-                    <svg
-                    xmlns="http://www.w3.org/2000/svg" 
-                    aria-hidden="true" 
-                    role="img" 
-                    width="1em" 
-                    height="1em" 
-                    className="nav-bar__svg svg-substack"
-                    viewBox="0 0 24 24"
-                    >
-
-                        <path 
-                        d="M22.539 8.242H1.46V5.406h21.08v2.836zM1.46 10.812V24L12 
-                        18.11L22.54 24V10.812H1.46zM22.54 0H1.46v2.836h21.08V0z" 
-                        fill="currentColor"/>
+                        <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="nav-bar__svg svg-instagram"
+                        viewBox="0 0 24 24"
+                        >
+                            <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                            <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37z"></path>
+                            <path d="M17.5 6.5L17.51 6.5"></path>
                         
-                    </svg>
+                        </svg>
+                    </a>
 
-                    <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="nav-bar__svg svg-facebook"
-                    viewBox="0 0 24 24"
+                    <a 
+                    target="_blank" 
+                    rel="noreferrer" 
+                    href="https://hamzafoy.substack.com/"
                     >
+                        <svg
+                        xmlns="http://www.w3.org/2000/svg" 
+                        aria-hidden="true" 
+                        role="img" 
+                        width="1em" 
+                        height="1em" 
+                        className="nav-bar__svg svg-substack"
+                        viewBox="0 0 24 24"
+                        >
 
-                        <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 
-                        0 011-1h3z"></path>
-                    
-                    </svg>
+                            <path 
+                            d="M22.539 8.242H1.46V5.406h21.08v2.836zM1.46 10.812V24L12 
+                            18.11L22.54 24V10.812H1.46zM22.54 0H1.46v2.836h21.08V0z" 
+                            fill="currentColor"/>
+                            
+                        </svg>
+                    </a>
+
+                    <a 
+                    target="_blank" 
+                    rel="noreferrer" 
+                    href="https://www.facebook.com/ihya502"
+                    >
+                        <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="nav-bar__svg svg-facebook"
+                        viewBox="0 0 24 24"
+                        >
+
+                            <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 
+                            0 011-1h3z"></path>
+                        
+                        </svg>
+                    </a>
 
                 </section>
 
