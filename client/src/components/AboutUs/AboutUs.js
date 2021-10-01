69111698145958e6d84de060e68071bee17ec8fe
Ross Foy <hamzaoflouisville@gmail.com>
2021-10-01 04:17:23 -0400

    finished mobile layout for AboutUs Component.

--- client/src/components/AboutUs/AboutUs.js
+++ client/src/components/AboutUs/AboutUs.js
@@ -5,6 +5,7 @@
 import './AboutUs.css';
 import NavBar from '../NavBar/NavBar';
 import React from 'react';
+import podcastArt from '../../assets/podcast.svg';
 
 
 
@@ -21,6 +22,37 @@ class AboutUs extends React.Component {
             <>
 
                 <div className="about-us-container">
+
+                    <section className="about-us__content">
+
+                        <h1 className="content__heading">
+                            What is Ihya502?
+                        </h1>
+
+                        <p className="content__text">
+                            Ihya502 is a podcast produced for the Louisville
+                            Muslim Community. The podcast's host is Br. Hamza
+                            Foy &amp; some episodes are co-hosted with Mishkat
+                            Suleiman. This podcast is intended to bring together
+                            local news, intelligent conversations, &amp; a revival
+                            in interest in a well-rounded Islamic understanding.
+                        </p>
+
+                        <p className="content__text">
+                            Episodes of Ihya502 will typically contain news 
+                            announcements and topics range between community 
+                            interviews, discussion of the Islamic sciences or local
+                            issues, &amp; reviews of literature or media. Guests are 
+                            always welcome to join and get behind the mic!
+                        </p>
+
+                    </section>
+
+                    <img 
+                    src={podcastArt}
+                    alt="Art of podcast being recorded."
+                    className="about-us__art"
+                    />
 
                 </div>
 
