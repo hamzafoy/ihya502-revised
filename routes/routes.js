bdc07dc51aa6a6100db3d60268c6e4e353154c3b
Ross Foy <hamzaoflouisville@gmail.com>
2021-10-09 23:45:57 -0400

    attempting to fix issue with async handling that may be potential source of problem with app.

--- routes/routes.js
+++ routes/routes.js
@@ -6,17 +6,33 @@ const express = require('express');
 const router = express.Router();
 const Feedback = require('../models/Feedback');
 
+function asyncHandler(cb) {
+    return async(req, res, next) => {
+        try {
+            await cb(req, res, next)
+        } catch(error) {
+            next(error);
+        }
+    }
+}
+
 
 
 /*::::::::::::::::::::::::::::::::::::::::
 :::::::::::  Backend Routes  :::::::::::::
 ::::::::::::::::::::::::::::::::::::::::*/
 
-router.post('/', (req, res) => {
+router.post('/', asyncHandler( async (req, res) => {
     console.log(req.body);
-    Feedback.create(req.body);
-    res.end();
-});
+    let feedback;
+    try {
+        Feedback.create(req.body);
+        console.log(`Feedback successfully submitted!`);
+        res.end();
+    } catch (error) {
+        throw error;
+    }
+}));
 
 
 
