a262c58edb7056442575bd060cc3c716affb1aff
Ross Foy <hamzaoflouisville@gmail.com>
2022-01-20 20:32:59 -0500

    Attempting to resolve issues with connecting to database.

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@
 
 const mongoose = require('mongoose');
 const config = require('config');
-const dbKey = process.env.MONGODB_URI || config.get('mongoURI');
+const dbKey = process.env.MONGODB_URI //|| config.get('mongoURI');
 
 
 
